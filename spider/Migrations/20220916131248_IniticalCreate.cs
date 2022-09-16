bfcf1089f66c32dc33c033f0f2c680524a1d4365
lucasguo <lucasguo@autuni.ac.nz>
2022-09-17 01:15:21 +1200

    delete records before running the program

---
+++ spider/Migrations/20220916131248_IniticalCreate.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace spider.Migrations
+{
+    /// <inheritdoc />
+    public partial class IniticalCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Sku = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    ImageLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Abv = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BottleSize = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CollectionDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ spider/Utils/Key.cs
@@ -0,0 +1,7 @@
+namespace spider.Utils;
+
+public class Key
+{ 
+    public static string User = "bHVjYXNndW84MTE=";
+    public static string Psw = "R2p4XzE5OTY4MjU=";
+}
